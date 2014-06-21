54d0a6fefa2a60bddbd8411488ef13b4cf37d9fb
Felix Kaaman <trundmatu@gmail.com>
2014-06-22 00:29:46 +0200

    Add executable

--- lib/rubatar.rb
+++ lib/rubatar.rb
@@ -1,15 +1,2 @@
 require 'rubatar/version'
-require 'optparse'
-
-if __FILE__ == $0
-  OptionParser.new do |o|
-    o.on('-w WIDTH') { |w| $width = w }
-    o.on('-h HEIGHT') { |h| $height = h }
-    o.on('-o OUTPUT') { |output| $output = output }
-    o.on('--help') { puts o; exit }
-    o.parse!
-  end
-
-  avatar = Rubatar::Avatar.new($width, $height, $output)
-  avatar.generate
-end
+require 'rubatar/avatar'
